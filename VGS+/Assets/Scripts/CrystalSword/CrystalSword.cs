14ca6338b5b8f5ac23708e466ff7ad9cb26af0cf
EnriqueNavarro <enrique1nta@gmail.com>
2018-10-01 11:23:31 -0500

    Crystal sword
    1 passive and auto attack programmed

---
+++ VGS+/Assets/Scripts/CrystalSword/CrystalSword.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalSword : Class {
+    [SerializeField] private float[] bloodShards;
+    public bool testCost;
+    public float tCost;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(testCost) {
+            expendShard(tCost);
+            testCost = false;
+        }
+	}
+    public bool expendShard(float cost) {        
+        float total=0;
+        foreach(float shard in bloodShards) {
+            total += shard;
+        }
+        if (total < cost) return false;
+        for (int i = 0; i < bloodShards.Length; i++)
+        {
+            if (bloodShards[i] > 0 && cost > 0)
+            {
+                if (cost >= bloodShards[i] && bloodShards[i]>0)
+                {
+                    cost -= bloodShards[i];
+                    bloodShards[i] = 0;
+                }
+                else
+                {
+                    bloodShards[i] -= cost;
+                    cost = 0;
+                }
+            }
+
+        }
+        return cost == 0;
+    }
+    public void generateShard(float generated) {
+        Debug.Log("Generating");
+        for(int i=0; i<bloodShards.Length;i++) {
+            if(bloodShards[i]<1 && generated>0) {
+                float toFill = 1 - bloodShards[i];
+                if (generated < toFill) {
+                    bloodShards[i] += generated;
+                    generated = 0;
+                } else {
+                    bloodShards[i] = 1;
+                    generated -= toFill;                    
+                }
+            }
+        }
+    }
+}
---
+++ VGS+/Assets/Scripts/CrystalSword/CrystalSwordAttack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalSwordAttack : Attack {
+    [SerializeField] private GameObject resource;
+    [SerializeField] private float generate;
+	
+	
+
+    // Update is called once per frame
+    public override void Activate()
+    {
+        foreach (GameObject enemy in enemies)
+        {
+            enemy.GetComponent<EnemyHealth>().damage(Damage);
+            resource.GetComponent<CrystalSword>().generateShard(generate);
+        }
+    }
+}
