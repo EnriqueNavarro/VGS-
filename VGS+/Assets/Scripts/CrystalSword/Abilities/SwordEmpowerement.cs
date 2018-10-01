a772951c10a7823d0414fde6e922cd751b971fa5
Enrique <enrique1nta@gmail.com>
2018-10-01 15:52:02 -0500

    Sword Empowerement

---
+++ VGS+/Assets/Scripts/CrystalSword/Abilities/SwordEmpowerement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordEmpowerement : Ability {
+    [SerializeField] private GameObject autoAttack;
+    [SerializeField] private float attackSpeed;
+    public override void Activate()
+    {
+        autoAttack.GetComponent<CrystalSwordAttack>().changer(Damage, Duration, sStats.Damage);
+        autoAttack.GetComponent<CrystalSwordAttack>().changer(attackSpeed, Duration, sStats.CD);
+    }
+}
