c66b860bee470a6694e20ecbbdf945455c07a484
Enrique <enrique1nta@gmail.com>
2018-10-01 15:24:45 -0500

    flurry

---
+++ VGS+/Assets/Scripts/CrystalSword/Abilities/Flurry.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flurry : Ability {
+    
+
+    // Update is called once per frame
+    public override void Activate()
+    {
+        foreach (GameObject enemy in enemies)
+        {
+            enemy.GetComponent<EnemyHealth>().damage(Damage, DmgType);
+        }
+    }
+}
