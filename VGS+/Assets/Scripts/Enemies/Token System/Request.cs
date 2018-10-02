0a7caf5ea5d4225387d477b0f894142b4b65b8ec
Enrique <enrique1nta@gmail.com>
2018-10-02 17:54:13 -0500

    token advancements

---
+++ VGS+/Assets/Scripts/Enemies/Token System/Request.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Request : MonoBehaviour {
+    public GameObject requester;// who is making the request
+    public bool inRange;// if the requester is in range to do the attack, only applicable to melle
+    public bool LOS;//if the requester would be able to shoot and hit the target
+    public int toHit;// how hard the hit is going to be for
+    public float distance;//distance to target
+    public int threat;//how much threat does the requester have on its target
+    public int targetHP;//how much hp does the target have
+    public int priority;//used to make a certain enemy have priority, the higher the number the more important
+    public float totalValue=0;
+    public int cost;
+
+    public Request(GameObject requester, bool inRange, bool lOS, int toHit, float distance, int threat, int targetHP)
+    {
+        this.requester = requester;
+        this.inRange = inRange;
+        LOS = lOS;
+        this.toHit = toHit;
+        this.distance = distance;
+        this.threat = threat;
+        this.targetHP = targetHP;
+        if (!inRange && !LOS) totalValue -= int.MaxValue;
+        totalValue += toHit;
+        totalValue += 10 + 1 / distance;
+        totalValue += threat;
+        totalValue += targetHP * 2;
+    }
+
+    public Request(GameObject requester, bool inRange, bool lOS, int toHit, float distance, int threat, int targetHP, int priority)
+    {
+        this.requester = requester;
+        this.inRange = inRange;
+        LOS = lOS;
+        this.toHit = toHit;
+        this.distance = distance;
+        this.threat = threat;
+        this.targetHP = targetHP;
+        this.priority = priority;
+        if (!inRange && !LOS) totalValue -= int.MaxValue;
+        totalValue += toHit;
+        totalValue += 10 + 1 / distance;
+        totalValue += threat;
+        totalValue += targetHP * 2;
+        totalValue += priority*1000;
+
+    }
+}
---
+++ VGS+/Assets/Scripts/Enemies/Token System/TokenManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TokenManager : MonoBehaviour {
+    [SerializeField] private List<GameObject> enemies = new List<GameObject>();//all enemies when spawn are added to this list
+    [SerializeField] private int maxTokens;
+    [SerializeField] private int currentTokens;
+    [SerializeField] private float timer;
+    [SerializeField] private List<Request> requests = new List<Request>();
+    [SerializeField] private List<Request> sorted = new List<Request>();
+    private int iterator=0;
+	// Use this for initialization
+	void Start () {
+        currentTokens = maxTokens;
+	}
+	public void Adder(GameObject e)//when an enemy spawns it must be added here
+    {
+        enemies.Add(e);
+    }
+    public void remover(GameObject e)//when an enemy dies, it must be eliminated here
+    {
+        enemies.Remove(e);
+    }
+    public void AddRequest(Request r)
+    {
+        requests.Add(r);
+    }
+    // Update is called once per frame
+    void FixedUpdate () {
+        iterator++;
+        if (iterator % 5 == 0) CheckRequests();
+	}
+    private Request Compare(Request r1, Request r2)
+    {
+        if (r1.totalValue >= r2.totalValue)return r1;
+        return r2;
+    }
+    private void Sort()
+    {
+        Request r1=null;
+        Request r2=null;
+        Request r3=null;
+        for (int i = 0; i < requests.Count; i++)
+        {
+            
+            for(int j =0; j < sorted.Count; j++)
+            {
+
+            }
+            if (sorted.Count == 0) sorted.Add(requests[i]);
+
+
+
+        }
+
+    }
+    private void CheckRequests()
+    {
+        
+        
+    }
+}
