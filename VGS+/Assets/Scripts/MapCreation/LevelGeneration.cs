5d50ba3acfca2dd9af8dedd71a073f8c7b6b4fa9
Mau <reverendo_siegfried@live.com.mx>
2018-10-11 14:39:11 -0500

    Generacion de mapa en plano x,z y arreglo de tamaños de sprites
    Cambios en la scene de Map Gen en pasar la generacion sobre xy a xz, y modificaciones en los pixel per unit de los personajes, para que quedaran conforme a una buena escala.
    Comienzo de Union de el mapa con los personajes, funciona bien todo sin errores

--- VGS+/Assets/Scripts/MapCreation/LevelGeneration.cs
+++ VGS+/Assets/Scripts/MapCreation/LevelGeneration.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGeneration : MonoBehaviour {
-	Vector2 worldSize = new Vector2(4,4);
+	Vector3 worldSize = new Vector3(4,0,4);
 	Room[,] rooms;
-	List<Vector2> takenPositions = new List<Vector2>();
-	int gridSizeX, gridSizeY, numberOfRooms = 20;
+	List<Vector3> takenPositions = new List<Vector3>();
+	int gridSizeX, gridSizeZ; 
+	public int numberOfRooms = 20;
 	public GameObject roomWhiteObj;
 	public Transform mapRoot;
 	void Start () {
-		if (numberOfRooms >= (worldSize.x * 2) * (worldSize.y * 2)){ // make sure we dont try to make more rooms than can fit in our grid
-			numberOfRooms = Mathf.RoundToInt((worldSize.x * 2) * (worldSize.y * 2));
+		if (numberOfRooms >= (worldSize.x * 2) * (worldSize.z * 2)){ // make sure we dont try to make more rooms than can fit in our grid
+			numberOfRooms = Mathf.RoundToInt((worldSize.x * 2) * (worldSize.z * 2));
 		}
 		gridSizeX = Mathf.RoundToInt(worldSize.x); //note: these are half-extents
-		gridSizeY = Mathf.RoundToInt(worldSize.y);
+		gridSizeZ = Mathf.RoundToInt(worldSize.z);
 		CreateRooms(); //lays out the actual map
 		SetRoomDoors(); //assigns the doors where rooms would connect
 		DrawMap(); //instantiates objects to make up a map
@@ -22,10 +23,10 @@ public class LevelGeneration : MonoBehaviour {
 	}
 	void CreateRooms(){
 		//setup
-		rooms = new Room[gridSizeX * 2,gridSizeY * 2];
-		rooms[gridSizeX,gridSizeY] = new Room(Vector2.zero, 1);
-		takenPositions.Insert(0,Vector2.zero);
-		Vector2 checkPos = Vector2.zero;
+		rooms = new Room[gridSizeX * 2,gridSizeZ * 2];
+		rooms[gridSizeX,gridSizeZ] = new Room(Vector3.zero, 1);
+		takenPositions.Insert(0,Vector3.zero);
+		Vector3 checkPos = Vector3.zero;
 		//magic numbers
 		float randomCompare = 0.2f, randomCompareStart = 0.2f, randomCompareEnd = 0.01f;
 		//add rooms
@@ -45,24 +46,24 @@ public class LevelGeneration : MonoBehaviour {
 					print("error: could not create with fewer neighbors than : " + NumberOfNeighbors(checkPos, takenPositions));
 			}
 			//finalize position
-			rooms[(int) checkPos.x + gridSizeX, (int) checkPos.y + gridSizeY] = new Room(checkPos, 0);
+			rooms[(int) checkPos.x + gridSizeX, (int) checkPos.z + gridSizeZ] = new Room(checkPos,0);
 			takenPositions.Insert(0,checkPos);
 		}	
 	}
-	Vector2 NewPosition(){
-		int x = 0, y = 0;
-		Vector2 checkingPos = Vector2.zero;
+	Vector3 NewPosition(){
+		int x = 0, z = 0;
+		Vector3 checkingPos = Vector3.zero;
 		do{
 			int index = Mathf.RoundToInt(Random.value * (takenPositions.Count - 1)); // pick a random room
 			x = (int) takenPositions[index].x;//capture its x, y position
-			y = (int) takenPositions[index].y;
+			z = (int) takenPositions[index].z;
 			bool UpDown = (Random.value < 0.5f);//randomly pick wether to look on hor or vert axis
 			bool positive = (Random.value < 0.5f);//pick whether to be positive or negative on that axis
 			if (UpDown){ //find the position bnased on the above bools
 				if (positive){
-					y += 1;
+					z += 1;
 				}else{
-					y -= 1;
+					z -= 1;
 				}
 			}else{
 				if (positive){
@@ -71,14 +72,14 @@ public class LevelGeneration : MonoBehaviour {
 					x -= 1;
 				}
 			}
-			checkingPos = new Vector2(x,y);
-		}while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY); //make sure the position is valid
+			checkingPos = new Vector3(x,0,z);
+		}while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || z >= gridSizeZ || z < -gridSizeZ); //make sure the position is valid
 		return checkingPos;
 	}
-	Vector2 SelectiveNewPosition(){ // method differs from the above in the two commented ways
+	Vector3 SelectiveNewPosition(){ // method differs from the above in the two commented ways
 		int index = 0, inc = 0;
-		int x =0, y =0;
-		Vector2 checkingPos = Vector2.zero;
+		int x =0, z =0;
+		Vector3 checkingPos = Vector3.zero;
 		do{
 			inc = 0;
 			do{ 
@@ -88,14 +89,14 @@ public class LevelGeneration : MonoBehaviour {
 				inc ++;
 			}while (NumberOfNeighbors(takenPositions[index], takenPositions) > 1 && inc < 100);
 			x = (int) takenPositions[index].x;
-			y = (int) takenPositions[index].y;
+			z = (int) takenPositions[index].z;
 			bool UpDown = (Random.value < 0.5f);
 			bool positive = (Random.value < 0.5f);
 			if (UpDown){
 				if (positive){
-					y += 1;
+					z += 1;
 				}else{
-					y -= 1;
+					z -= 1;
 				}
 			}else{
 				if (positive){
@@ -104,25 +105,25 @@ public class LevelGeneration : MonoBehaviour {
 					x -= 1;
 				}
 			}
-			checkingPos = new Vector2(x,y);
-		}while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY);
+			checkingPos = new Vector3(x,0,z);
+		}while (takenPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || z >= gridSizeZ || z < -gridSizeZ);
 		if (inc >= 100){ // break loop if it takes too long: this loop isnt garuanteed to find solution, which is fine for this
 			print("Error: could not find position with only one neighbor");
 		}
 		return checkingPos;
 	}
-	int NumberOfNeighbors(Vector2 checkingPos, List<Vector2> usedPositions){
+	int NumberOfNeighbors(Vector3 checkingPos, List<Vector3> usedPositions){
 		int ret = 0; // start at zero, add 1 for each side there is already a room
-		if (usedPositions.Contains(checkingPos + Vector2.right)){ //using Vector.[direction] as short hands, for simplicity
+		if (usedPositions.Contains(checkingPos + Vector3.right)){ //using Vector.[direction] as short hands, for simplicity
 			ret++;
 		}
-		if (usedPositions.Contains(checkingPos + Vector2.left)){
+		if (usedPositions.Contains(checkingPos + Vector3.left)){
 			ret++;
 		}
-		if (usedPositions.Contains(checkingPos + Vector2.up)){
+		if (usedPositions.Contains(checkingPos + Vector3.forward)){
 			ret++;
 		}
-		if (usedPositions.Contains(checkingPos + Vector2.down)){
+		if (usedPositions.Contains(checkingPos + Vector3.back)){
 			ret++;
 		}
 		return ret;
@@ -132,11 +133,11 @@ public class LevelGeneration : MonoBehaviour {
 			if (room == null){
 				continue; //skip where there is no room
 			}
-			Vector2 drawPos = room.gridPos;
+			Vector3 drawPos = room.gridPos;
 			drawPos.x *= 16;//aspect ratio of map sprite
-			drawPos.y *= 8;
+			drawPos.z *= 8;
 			//create map obj and assign its variables
-			MapSpriteSelector mapper = Object.Instantiate(roomWhiteObj, drawPos, Quaternion.identity).GetComponent<MapSpriteSelector>();
+			MapSpriteSelector mapper = Object.Instantiate(roomWhiteObj, drawPos, Quaternion.Euler(90,0,0)).GetComponent<MapSpriteSelector>();
 			mapper.type = room.type;
 			mapper.up = room.doorTop;
 			mapper.down = room.doorBot;
@@ -147,30 +148,30 @@ public class LevelGeneration : MonoBehaviour {
 	}
 	void SetRoomDoors(){
 		for (int x = 0; x < ((gridSizeX * 2)); x++){
-			for (int y = 0; y < ((gridSizeY * 2)); y++){
-				if (rooms[x,y] == null){
+			for (int z = 0; z < ((gridSizeZ * 2)); z++){
+				if (rooms[x,z] == null){
 					continue;
 				}
-				Vector2 gridPosition = new Vector2(x,y);
-				if (y - 1 < 0){ //check above
-					rooms[x,y].doorBot = false;
+				Vector3 gridPosition = new Vector3(x,0,z);
+				if (z - 1 < 0){ //check above
+					rooms[x,z].doorBot = false;
 				}else{
-					rooms[x,y].doorBot = (rooms[x,y-1] != null);
+					rooms[x,z].doorBot = (rooms[x,z-1] != null);
 				}
-				if (y + 1 >= gridSizeY * 2){ //check bellow
-					rooms[x,y].doorTop = false;
+				if (z + 1 >= gridSizeZ * 2){ //check bellow
+					rooms[x,z].doorTop = false;
 				}else{
-					rooms[x,y].doorTop = (rooms[x,y+1] != null);
+					rooms[x,z].doorTop = (rooms[x,z+1] != null);
 				}
 				if (x - 1 < 0){ //check left
-					rooms[x,y].doorLeft = false;
+					rooms[x,z].doorLeft = false;
 				}else{
-					rooms[x,y].doorLeft = (rooms[x - 1,y] != null);
+					rooms[x,z].doorLeft = (rooms[x - 1,z] != null);
 				}
 				if (x + 1 >= gridSizeX * 2){ //check right
-					rooms[x,y].doorRight = false;
+					rooms[x,z].doorRight = false;
 				}else{
-					rooms[x,y].doorRight = (rooms[x+1,y] != null);
+					rooms[x,z].doorRight = (rooms[x+1,z] != null);
 				}
 			}
 		}
--- VGS+/Assets/Scripts/MapCreation/RoomInstance.cs
+++ VGS+/Assets/Scripts/MapCreation/RoomInstance.cs
@@ -14,8 +14,8 @@ public class RoomInstance : MonoBehaviour {
 	[SerializeField]
 	ColorToGameObject[] mappings;
 	float tileSize = 16;
-	Vector2 roomSizeInTiles = new Vector2(9,17);
-	public void Setup(Texture2D _tex, Vector2 _gridPos, int _type, bool _doorTop, bool _doorBot, bool _doorLeft, bool _doorRight){
+	Vector3 roomSizeInTiles = new Vector3(9,0,17);
+	public void Setup(Texture2D _tex, Vector3 _gridPos, int _type, bool _doorTop, bool _doorBot, bool _doorLeft, bool _doorRight){
 		tex = _tex;
 		gridPos = _gridPos;
 		type = _type;
@@ -28,10 +28,10 @@ public class RoomInstance : MonoBehaviour {
 	}
 	void MakeDoors(){
 		//top door, get position then spawn
-		Vector3 spawnPos = transform.position + Vector3.up*(roomSizeInTiles.y/4 * tileSize) - Vector3.up*(tileSize/4);
+		Vector3 spawnPos = transform.position + Vector3.forward*(roomSizeInTiles.z/4 * tileSize) - Vector3.forward*(tileSize/4);
 		PlaceDoor(spawnPos, doorTop, doorU);
 		//bottom door
-		spawnPos = transform.position + Vector3.down*(roomSizeInTiles.y/4 * tileSize) - Vector3.down*(tileSize/4);
+		spawnPos = transform.position + Vector3.back*(roomSizeInTiles.z/4 * tileSize) - Vector3.back*(tileSize/4);
 		PlaceDoor(spawnPos, doorBot, doorD);
 		//right door
 		spawnPos = transform.position + Vector3.right*(roomSizeInTiles.x * tileSize) - Vector3.right*(tileSize);
@@ -43,21 +43,21 @@ public class RoomInstance : MonoBehaviour {
 	void PlaceDoor(Vector3 spawnPos, bool door, GameObject doorSpawn){
 		// check whether its a door or wall, then spawn
 		if (door){
-			Instantiate(doorSpawn, spawnPos, Quaternion.identity).transform.parent = transform;
+			Instantiate(doorSpawn, spawnPos, Quaternion.Euler(-90,0,0)).transform.parent = transform;
 		}else{
-			Instantiate(doorWall, spawnPos, Quaternion.identity).transform.parent = transform;
+			Instantiate(doorWall, spawnPos, Quaternion.Euler(-90,0,0)).transform.parent = transform;
 		}
 	}
 	void GenerateRoomTiles(){
 		//loop through every pixel of the texture
 		for(int x = 0; x < tex.width; x++){
-			for (int y = 0; y < tex.height; y++){
-				GenerateTile(x,y);
+			for (int z = 0; z < tex.height; z++){
+				GenerateTile(x,z);
 			}
 		}
 	}
-	void GenerateTile(int x, int y){
-		Color pixelColor = tex.GetPixel(x,y);
+	void GenerateTile(int x, int z){
+		Color pixelColor = tex.GetPixel(x,z);
 		//skip clear spaces in texture
 		if (pixelColor.a == 0){
 			return;
@@ -65,20 +65,20 @@ public class RoomInstance : MonoBehaviour {
 		//find the color to math the pixel
 		foreach (ColorToGameObject mapping in mappings){
 			if (mapping.color.Equals(pixelColor)){
-				Vector3 spawnPos = positionFromTileGrid(x,y);
-				Instantiate(mapping.prefab, spawnPos, Quaternion.identity).transform.parent = this.transform;
+				Vector3 spawnPos = positionFromTileGrid(x,z);
+				Instantiate(mapping.prefab, spawnPos, Quaternion.Euler(-90,0,0)).transform.parent = this.transform;
 			}else{
 				//forgot to remove the old print for the tutorial lol so I'll leave it here too
 				//print(mapping.color + ", " + pixelColor);
 			}
 		}
 	}
-	Vector3 positionFromTileGrid(int x, int y){
+	Vector3 positionFromTileGrid(int x, int z){
 		Vector3 ret;
 		//find difference between the corner of the texture and the center of this object
-		Vector3 offset = new Vector3((-roomSizeInTiles.x + 1)*tileSize, (roomSizeInTiles.y/4)*tileSize - (tileSize/4), 0);
+		Vector3 offset = new Vector3((-roomSizeInTiles.x + 1)*tileSize, 0, (roomSizeInTiles.z/4)*tileSize - (tileSize/4));
 		//find scaled up position at the offset
-		ret = new Vector3(tileSize * (float) x, -tileSize * (float) y, 0) + offset + transform.position;
+		ret = new Vector3(tileSize * (float) x, 0, -tileSize * (float) z) + offset + transform.position;
 		return ret;
 	}
 }
