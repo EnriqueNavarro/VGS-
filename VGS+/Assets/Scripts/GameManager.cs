15d0ba98dfc60357d446cf1c8dd50128a297f706
Andresbd <andres.bd@outlook.com>
2018-10-18 10:25:29 -0500

    Main-Menu and loading screens
    Added an interface manager that can handle scene preloading and releasing the scene when ready.

---
+++ VGS+/Assets/Scripts/GameManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+	public static GameManager instance = null;
+
+	private string menu = "MainMenu";
+	private SaveData SaveData;
+	private string saveName = "save.json";
+
+	void Awake()
+	{
+		
+		if (instance == null)
+		{
+			instance = this;
+		}else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+		
+		DontDestroyOnLoad(gameObject);
+		
+		LoadGameData();
+	}
+	
+	public void EndGame()
+	{
+		SaveProgress();
+		SceneManager.LoadScene("ScoreScreen");
+	}
+	
+	private void SaveProgress()
+	{
+		//Create a new Save data
+		SaveData sD = new SaveData();
+
+		//Assign current status to that saved data
+		//Declare what will be saved of the player
+        
+		//Parse it to JSON
+		string dataJson = JsonUtility.ToJson(sD);
+		string saveState = Path.Combine(Application.persistentDataPath, saveName);
+
+		//Write file
+		File.WriteAllText(saveState, dataJson);
+	}
+
+	//Load current data from saved data
+	private void LoadGameData()
+	{
+		string filePath = Path.Combine(Application.persistentDataPath, saveName);
+        
+		if (File.Exists(filePath))
+		{
+			string dataJson = File.ReadAllText(filePath);
+            
+			SaveData loadData = JsonUtility.FromJson<SaveData>(dataJson);
+
+			//Load stored data into current execution
+		}
+		else
+		{
+			Debug.LogError("No Save File");   
+		}
+	}
+
+
+}
---
+++ VGS+/Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+	public void LoadMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void LoadTest()
+	{
+		SceneManager.LoadScene("Test");
+	}
+
+	public void LoadRunes()
+	{
+		SceneManager.LoadScene("Runes");
+	}
+}
