499ef4c2007ee7e0b56bad91a1fc37f2cd7f2a35
Andresbd <andres.bd@outlook.com>
2018-10-21 19:46:59 -0500

    Fixed movement horientation and animations

--- VGS+/Assets/Scripts/PlayerMovement.cs
+++ VGS+/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     [SerializeField] private Vector3 velocity;
     [SerializeField] private float speedModifier = 1;
+    private float movement;
+    private bool facingRight;
+    public Animator animator;
     
 
     public float SpeedModifier
@@ -22,8 +25,26 @@ public class PlayerMovement : MonoBehaviour {
     
     private void Update()
     {
+        
+
+        if (Input.GetAxis("Horizontal") < 0 && facingRight) {
+            flip();
+        }
+
+        if (Input.GetAxis("Horizontal") > 0 && !facingRight)
+        {
+            flip();
+        }
         velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         this.transform.position += velocity*SpeedModifier;
+
+        movement = (velocity.x + velocity.z) * SpeedModifier;
+        animator.SetFloat("Speed", Mathf.Abs(movement));
       
+    }
+
+    void flip() {
+        facingRight = !facingRight;
+        transform.Rotate(Vector3.forward * 180);
     }
 }
