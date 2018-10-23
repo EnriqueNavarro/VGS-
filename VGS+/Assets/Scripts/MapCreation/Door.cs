283a043b5c6f59c1b7db8d4192f285e14eca1e34
Mau <reverendo_siegfried@live.com.mx>
2018-10-22 21:38:50 -0500

    Main Camera movimiento completado
    Ya se mueven las dos camaras fluidamente

--- VGS+/Assets/Scripts/MapCreation/Door.cs
+++ VGS+/Assets/Scripts/MapCreation/Door.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class Door : MonoBehaviour {
 	[SerializeField]
 	MapCamera camera;
+	[SerializeField]
+	TargetMovement target;
 
 	void Start () {
 		camera = FindObjectOfType <MapCamera> ();
+		target = FindObjectOfType <TargetMovement> ();
 	}
 
 	void OnTriggerEnter(Collider other){
 		if(other.CompareTag("Player")){
 			other.transform.position = transform.GetChild (0).position;
 			camera.Move (gameObject);
+			target.Move (gameObject);
 		}
 	}
 }
