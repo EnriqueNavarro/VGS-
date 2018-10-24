23ffbf558980f77f51faafe1a31109fe893b4bab
EnriqueNavarro <enrique1nta@gmail.com>
2018-10-24 11:56:05 -0500

    Orbiting crystals + animations

---
+++ VGS+/Assets/Scripts/CrystalSword/OrbitCrystals.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCrystals : MonoBehaviour {
+    [SerializeField] private GameObject father;
+    [SerializeField] private float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	void OrbitAround() {
+        transform.RotateAround(father.transform.position, new Vector3(0,0,1), speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(90,0,0);
+    }
+	// Update is called once per frame
+	void Update () {
+        OrbitAround();
+	}
+}
