41410690292ee9beb3875346b26bac0e6479ba48
EnriqueNavarro <enrique1nta@gmail.com>
2018-10-24 14:23:02 -0500

    Ranged Enemy Movement

--- VGS+/Assets/Scripts/Enemies/EnemyRangedMovement.cs
+++ VGS+/Assets/Scripts/Enemies/EnemyRangedMovement.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour {
+public class EnemyRangedMovement : MonoBehaviour {
     [SerializeField] private bool combat;
     [SerializeField] private GameObject target;
     [SerializeField] private Vector3 lastKnownPos;
@@ -10,19 +10,20 @@ public class EnemyMovement : MonoBehaviour {
     [SerializeField] float maxDistanceDelta;
     [SerializeField] float minDistance;
     [SerializeField] private LayerMask lm;
-	
-	
-	// Update is called once per frame
-	void Update () {
+    
+    // Update is called once per frame
+    void Update()
+    {
         combat = this.GetComponent<EnemyHealth>().combat;
         if (this.GetComponent<EnemyHealth>().Attacker != null)
         {
             target = this.GetComponent<EnemyHealth>().Attacker;
-        } else
+        }
+        else
         {
             target = null;
         }
-	}
+    }
     private void FixedUpdate()
     {
         if (combat)
@@ -35,13 +36,13 @@ public class EnemyMovement : MonoBehaviour {
                 {
                     if (hit.transform == target.transform)
                     {
-                        if(Vector2.Distance(new Vector2(transform.position.x,transform.position.z),new Vector2(target.transform.position.x, target.transform.position.z))<minDistance)
+                        if (Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(target.transform.position.x, target.transform.position.z)) < minDistance)
                         {
                             //lastKnownPos = transform.position;
                         }
                         else
                         {
-                            lastKnownPos = target.transform.position;
+                            lastKnownPos = new Vector3(target.transform.position.x*-1, target.transform.position.y, target.transform.position.z*-1);
                         }
                     }
                     if (Input.GetKeyDown("m")) Debug.Log("Enemy position: " + transform.position + " Player position: " + target.transform.position + " Distance: " + Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(target.transform.position.x, target.transform.position.z))
@@ -53,8 +54,8 @@ public class EnemyMovement : MonoBehaviour {
                 transform.position = Vector3.MoveTowards(transform.position, lastKnownPos, maxDistanceDelta);
                 //Debug.Log("Going to " + lastKnownPos +"from"+transform.position+ " distance=" + Vector3.Distance(transform.position, target.transform.position));
             }
-            
+
         }
-        
+
     }
 }
