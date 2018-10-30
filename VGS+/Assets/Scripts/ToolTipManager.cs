87feab2089385c3860395c531eb29a448cbcfb9d
Enrique <enrique1nta@gmail.com>
2018-10-30 17:15:06 -0600

    Tooltips done

---
+++ VGS+/Assets/Scripts/ToolTipManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTipManager : MonoBehaviour {
+    public Text text;
+    public string CrystalSwordQ;
+    public void displayCrystalSwordQ()
+    {
+        text.text = CrystalSwordQ;
+    }
+    public string CrystalSwordW;
+    public void displayCrystalSwordW()
+    {
+        text.text = CrystalSwordW;
+    }
+    public string CrystalSwordE;
+    public void displayCrystalSwordE()
+    {
+        text.text = CrystalSwordE;
+    }
+    public string CrystalSwordR;
+    public void displayCrystalSwordR()
+    {
+        text.text = CrystalSwordR;
+    }
+    public string ShadowDancerQ;
+    public void displayShadowDancerQ()
+    {
+        text.text = ShadowDancerQ;
+    }
+    public string ShadowDancerW;
+    public void displayShadowDancerW()
+    {
+        text.text = ShadowDancerW;
+    }
+    public string ShadowDancerE;
+    public void displayShadowDancerE()
+    {
+        text.text = ShadowDancerE;
+    }
+    public string ShadowDancerR;
+    public void displayShadowDancerR()
+    {
+        text.text = ShadowDancerR;
+    }
+
+}
